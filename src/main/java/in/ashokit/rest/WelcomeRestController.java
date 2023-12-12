e9d751a80a4321dd612d45c7f51da21ec6c9e1cc
rbasha786 <139204364+rbasha786@users.noreply.github.com>
2023-12-12 15:06:12 +0530

    Update WelcomeRestController.java

--- src/main/java/in/ashokit/rest/WelcomeRestController.java
+++ src/main/java/in/ashokit/rest/WelcomeRestController.java
@@ -3,23 +3,22 @@ package in.ashokit.rest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeRestController {
 
-	private Logger logger = LoggerFactory.getLogger(WelcomeRestController.class);
+    private Logger logger = LoggerFactory.getLogger(WelcomeRestController.class);
 
-	public WelcomeRestController() {
-		logger.info("***** WelcomeRestController::Constructor *****");
-	}
+    public WelcomeRestController() {
+        logger.info("***** WelcomeRestController::Constructor *****");
+    }
 
-	@GetMapping
-	public String welcomeMsg() {
-		logger.info("***** welcomeMsg() execution start *****");
-		String msg = "Welcome to SrimanIT ---> https://srimanit.com/";
-		logger.info("***** welcomeMsg() execution end *****");
-		return msg;
-	}
+    @GetMapping
+    public String welcomeMsg() {
+        logger.info("***** welcomeMsg() execution start *****");
+        String msg = "Welcome to SrimanIT - Visit our website: https://srimanit.com/";
+        logger.info("***** welcomeMsg() execution end *****");
+        return msg;
+    }
 }
